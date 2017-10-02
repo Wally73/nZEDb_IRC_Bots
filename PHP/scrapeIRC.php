6fd7a41a0d580d9d8eca18a3d9ed301b64c28621
Wally73 <waltermeijer@gmail.com>
2017-10-02 09:12:10 +0200

    slight update

--- PHP/scrapeIRC.php
+++ PHP/scrapeIRC.php
@@ -5,7 +5,7 @@ if (!isset($argv[1]) || !in_array($argv[1], array('efnet', 'corrupt', 'opentrack
 }
 $silent = ((isset($argv[2]) && $argv[2] === 'true')  ? true : false);
 define('pre_settings', true);
-// how do i add opentracker to his if/else ? 
+// how do i add opentracker to this if/else ? 
 if ($argv[1] === 'efnet') {
 	define('efnet_bot_settings', true);
 } else {
